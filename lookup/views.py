013195bfc45a39672ac5c096010405cfd121d32b
fares siouane <fares.geek.dz@gmail.com>
2020-03-02 21:42:00 +0100

    set-up basic weather api with basic functionallity

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {'person' : 'fares'})
+    import json
+    import requests
+    #http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=DC937C79-8B92-4099-B0A3-13226AAA32DA
+    api_request = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=DC937C79-8B92-4099-B0A3-13226AAA32DA')
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+
+    return render(request, 'home.html', {'api' : api})
+
 
 def about(request):
     return render(request, 'about.html', {})
