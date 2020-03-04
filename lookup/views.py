a8d6768024968ea03985e8d764ee33cc0dfb929d
fares siouane <fares.geek.dz@gmail.com>
2020-03-04 22:29:25 +0100

    made a basic formating for the API results

--- lookup/views.py
+++ lookup/views.py
@@ -6,6 +6,7 @@ def home(request):
     import requests
     #http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=DC937C79-8B92-4099-B0A3-13226AAA32DA
     api_request = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=DC937C79-8B92-4099-B0A3-13226AAA32DA')
+    
     try:
         api = json.loads(api_request.content)
     except Exception as e:
